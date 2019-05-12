96bae2b074d720b4159cf5048e1efbde8ebe4600
CHUREKGIT <chur@amu.edu.pl>
2019-05-12 17:19:22 +0200

    polish by me

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField()
+    jobposition = models.CharField(max_length=200, default='paker')
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,5 +7,6 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name = 'home' ),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('<int:job_id>/', jobs.views.detail, name="job")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
